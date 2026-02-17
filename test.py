1239cf656ff72ba8d805323e190444ddfde91cd3
Myten88 <you@example.comtiinusmy@gmail.com>
2026-02-17 14:13:03 +0100

    fort. tester

---
+++ test.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+
+st.title("Min första Streamlit-app")
+
+df = pd.DataFrame({
+    "first column": [1, 2, 3, 4],
+    "second column": [10, 20, 30, 40],
+})
+
+st.dataframe(df)
