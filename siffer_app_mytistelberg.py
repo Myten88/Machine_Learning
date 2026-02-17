a7cc9021f30fac9fa526297749b120d1f0ca03aa
Myten88 <you@example.comtiinusmy@gmail.com>
2026-02-17 15:05:47 +0100

    Skapade och börjar skriva i kunskapstest wordfilen

--- siffer_app_mytistelberg.py
+++ siffer_app_mytistelberg.py
@@ -1,1 +1,8 @@
+import streamlit as st
+
+st.title("Skriv en siffra och låt modellen gissa")
+st.markdown("""Här nere kan du rita en siffra mellan 0-9 och låta modellen gissa vilken siffra du skrev.
+            Ju finare du skriver desto lättare kommer modellen kunna gissa. """)
+
+
 
