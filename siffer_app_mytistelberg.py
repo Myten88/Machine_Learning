dc4abc4c9eace07bc3332f6ffed9138c6439e4aa
Myten88 <you@example.comtiinusmy@gmail.com>
2026-02-18 13:49:53 +0100

    Testar drawable_canvas

--- siffer_app_mytistelberg.py
+++ siffer_app_mytistelberg.py
@@ -1,8 +1,18 @@
 import streamlit as st
+from streamlit_drawable_canvas import st_canvas
 
 st.title("Skriv en siffra och låt modellen gissa")
 st.markdown("""Här nere kan du rita en siffra mellan 0-9 och låta modellen gissa vilken siffra du skrev.
             Ju finare du skriver desto lättare kommer modellen kunna gissa. """)
+
+canvas_result = st_canvas()
+st_canvas(
+    stroke_width=(1, 25, 3),
+    stroke_color='black',
+    update_streamlit=True,
+    drawing_mode='freedraw',
+    key='canvas',
+)
 
 
 
